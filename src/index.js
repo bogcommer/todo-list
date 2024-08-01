b53aabfca03fccaf594305acfa16ba5b93bdb7df
bogcommer <beddoeschristian@gmail.com>
2024-08-01 16:26:34 +0100

    Added basic functionality for project lists

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
 import './style.css';
+const btn = document.querySelector(".project-btn");
+const list = document.querySelector(".list");
 
+function project() {
+    const element = document.createElement('div');
+    
+    element.classList.add = 'todo'
+    element.innerHTML = ''
+
+    return element;
+}
+
+btn.addEventListener("click", () => {
+    list.innerHTML = '';
+
+    list.innerHTML = `<div class="todo">
+        <h4 class="todo-header">todo</h4>
+        <p class="todo-desc">Description: blablabla</p>
+        <p class="todo-date">Date by: 03-04-24</p>
+        <p class="todo-priority">Priority: High</p>
+      </div>`;
+});
